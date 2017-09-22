16f7429ce4d8f8de85616dcd99b8e19cbd80eeaa
Sergey Plashenko <spl@sitecore.net>
2017-09-22 10:48:52 +0300

    Initial release

---
+++ src/Sitecore.Support.185487/SitecoreContentTestStore.cs
@@ -0,0 +1,57 @@
+﻿namespace Sitecore.Support.ContentTesting.Data
+{
+    using Sitecore.ContentTesting.ContentSearch;
+    using Sitecore.ContentTesting.ContentSearch.Models;
+    using Sitecore.ContentTesting.Helpers;
+    using Sitecore.Data;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class SitecoreContentTestStore : Sitecore.ContentTesting.Data.SitecoreContentTestStore
+    {
+        public override IEnumerable<SuggestedTestSearchResultItem> GetSuggestedTests(DataUri hostItemDataUri, string searchText = null)
+        {
+            Sitecore.Support.ContentTesting.ContentSearch.TestingSearch search = new Sitecore.Support.ContentTesting.ContentSearch.TestingSearch();
+            if (hostItemDataUri != null)
+            {
+                search.HostItem = hostItemDataUri;
+            }
+            if (!string.IsNullOrEmpty(searchText))
+            {
+                search.SearchText = searchText;
+            }
+            SuggestedTestSearchResultItem[] data = search.GetSuggestedTests().ToArray<SuggestedTestSearchResultItem>();
+            TestMetricsRange suggestedTestMetricsRange = search.GetSuggestedTestMetricsRange();
+            TestDataHelper.NormalizeTestData<SuggestedTestSearchResultItem>(data, r => r.Potential, delegate (SuggestedTestSearchResultItem r, double v)
+            {
+                r.Potential = Math.Ceiling(v);
+            }, 10.0, suggestedTestMetricsRange.PotentialMin, suggestedTestMetricsRange.PotentialMax);
+            TestDataHelper.NormalizeTestData<SuggestedTestSearchResultItem>(data, r => r.Impact, delegate (SuggestedTestSearchResultItem r, double v)
+            {
+                r.Impact = Math.Ceiling(v);
+            }, 10.0, suggestedTestMetricsRange.ImpactMin, suggestedTestMetricsRange.ImpactMax);
+            TestDataHelper.NormalizeTestData<SuggestedTestSearchResultItem>(data, r => r.Recommendation, delegate (SuggestedTestSearchResultItem r, double v)
+            {
+                r.Recommendation = Math.Ceiling(v);
+            }, 10.0, suggestedTestMetricsRange.RecommendationMin, suggestedTestMetricsRange.RecommendationMax);
+            data = (from x in data
+                    where ((x.Potential > 0.0) && (x.Impact > 0.0)) && (x.Recommendation > 0.0)
+                    select x).ToArray<SuggestedTestSearchResultItem>();
+            IEnumerable<TestingSearchResultItem> source = this.GetActiveTests(hostItemDataUri, searchText, null);
+            IEnumerable<ID> first = from x in data select x.ItemId;
+            IEnumerable<ID> second = source.Select<TestingSearchResultItem, ID>(delegate (TestingSearchResultItem x)
+            {
+                if (x.HostItemUri == null)
+                {
+                    return null;
+                }
+                return x.HostItemUri.ItemID;
+            });
+            IEnumerable<ID> validIds = first.Except<ID>(second);
+            return (from s in data
+                    where validIds.Contains<ID>(s.ItemId)
+                    select s);
+        }
+    }
+}
---
+++ src/Sitecore.Support.185487/TestingSearch.cs
@@ -0,0 +1,50 @@
+﻿using Sitecore.ContentTesting.ContentSearch;
+
+namespace Sitecore.Support.ContentTesting.ContentSearch
+{
+    using Diagnostics;
+    using Sitecore;
+    using Sitecore.ContentSearch;
+    using Sitecore.ContentSearch.Linq.Utilities;
+    using Sitecore.ContentSearch.Security;
+    using Sitecore.ContentTesting.ContentSearch.Models;
+    using System;
+    using System.Linq;
+    using System.Linq.Expressions;
+
+    public class TestingSearch : Sitecore.ContentTesting.ContentSearch.TestingSearch
+    {
+        public new TestMetricsRange GetSuggestedTestMetricsRange()
+        {
+            TestMetricsRange range = new TestMetricsRange();
+            ISearchIndex suggestedTestSearchIndex = GetSuggestedTestSearchIndex();
+            if (suggestedTestSearchIndex != null)
+            {
+                using (IProviderSearchContext context = suggestedTestSearchIndex.CreateSearchContext(SearchSecurityOptions.DisableSecurityCheck))
+                {
+                    Expression<Func<SuggestedTestSearchResultItem, bool>> first = PredicateBuilder.True<SuggestedTestSearchResultItem>();
+                    first = first.And<SuggestedTestSearchResultItem>(x => x.IsLatest);
+                    first = first.And<SuggestedTestSearchResultItem>(x => x.Language == Context.Language.Name);
+                    first = first.And<SuggestedTestSearchResultItem>(x => x.RawSuggestions != string.Empty);
+                    IQueryable<SuggestedTestSearchResultItem> queryable = context.GetQueryable<SuggestedTestSearchResultItem>().Where<SuggestedTestSearchResultItem>(first);
+                    range.ImpactMax = double.MinValue;
+                    range.ImpactMin = double.MaxValue;
+                    range.RecommendationMax = double.MinValue;
+                    range.RecommendationMin = double.MaxValue;
+                    range.PotentialMax = double.MinValue;
+                    range.PotentialMin = double.MaxValue;
+                    foreach (SuggestedTestSearchResultItem item in queryable)
+                    {
+                        if (item.Impact > range.ImpactMax) range.ImpactMax = item.Impact;
+                        if (item.Recommendation > range.RecommendationMax) range.RecommendationMax = item.Recommendation;
+                        if (item.Potential > range.PotentialMax) range.PotentialMax = item.Potential;
+                        if (item.Impact < range.ImpactMin) range.ImpactMin = item.Impact;
+                        if (item.Recommendation < range.RecommendationMin) range.RecommendationMin = item.Recommendation;
+                        if (item.Potential < range.PotentialMin) range.PotentialMin = item.Potential;
+                    }
+                }
+            }
+            return range;
+        }
+    }
+}
